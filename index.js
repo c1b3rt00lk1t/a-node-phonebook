f09d21fa6739b703bbc45a0463717523ff4feece
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2022-11-01 10:00:09 +0100

    Added missing name and number or duplicate name checks.

--- index.js
+++ index.js
@@ -1,67 +1,80 @@
-
-const express = require('express');
+const express = require("express");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons);
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  const person = persons.find((person) => person.id === id);
+  person ? response.json(person) : response.sendStatus(404).end();
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = +request.params.id;
-    const person = persons.find(person => person.id === id);
-    person ? response.json(person) : response.sendStatus(404).end();
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  const length = persons.length;
+  persons = persons.filter((person) => person.id !== id);
+  persons.length !== length
+    ? response.sendStatus(204).end()
+    : response.sendStatus(404).end();
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = +request.params.id;
-    const length = persons.length;
-    persons = persons.filter(person => person.id !== id);
-    persons.length !== length ? response.sendStatus(204).end() : response.sendStatus(404).end();
-})
+app.post("/api/persons/", (request, response) => {
+  const body = request.body;
 
-app.post('/api/persons/', (request, response) => {
-    const body = request.body;
- 
-    const id = Math.floor(Math.random() * 10000);
-    body.id = id;
-    persons.push(body);
-    response.json(body)
-})
+  if (!body) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  } else if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: "missing name or number" 
+    });
+  } else if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: "name must be unique"
+    });
+  }
 
-app.get('/info', (request, response) => {
-    const date = new Date();
-    const infoMessage = `<h1>Info</h1><p>Phonebook has info for ${persons.length} people.</p><p>${date}</p>`
-    response.sendStatus(infoMessage);
-})
+  const id = Math.floor(Math.random() * 10000);
+  body.id = id;
+  persons.push(body);
+  response.json(body);
+});
 
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const infoMessage = `<h1>Info</h1><p>Phonebook has info for ${persons.length} people.</p><p>${date}</p>`;
+  response.sendStatus(infoMessage);
+});
 
 const PORT = 3002;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
